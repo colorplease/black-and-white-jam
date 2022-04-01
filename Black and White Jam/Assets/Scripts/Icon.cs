8cedd3bbe3b148a6236c3fec3118570946bf9b72
colorplease <74271202+colorplease@users.noreply.github.com>
2022-04-01 12:16:39 -0700

    windows am i right? hahahahahahahahahahaha

--- Black and White Jam/Assets/Scripts/Icon.cs
+++ Black and White Jam/Assets/Scripts/Icon.cs
@@ -9,14 +9,25 @@ using UnityEngine.EventSystems;
 public class Icon : MonoBehaviour, IPointerClickHandler
 {
     int tap;
+    public GameObject windowToOpen;
+    public RectTransform Canvas;
     
      public void OnPointerClick(PointerEventData eventData)
     {
-        tap = eventData.clickCount;
+            tap = eventData.clickCount;
  
         if (tap == 2)
         {
-            Debug.Log("double clicked");
+            if (GameObject.Find(windowToOpen.name + "(Clone)") != null)
+            {
+                return;
+            }
+            else
+            {
+                GameObject window = Instantiate(windowToOpen, new Vector3(Random.Range(-100, 100), Random.Range(-87, 155), 0), transform.rotation);
+            window.transform.SetParent(Canvas, false);
+            }
+            
         }
  
     }
---
+++ Black and White Jam/Assets/Scripts/Window.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    public void Minimize()
+    {
+        Debug.Log("Minimize");
+    }
+
+    public void Close()
+    {
+        Destroy(gameObject);
+    }
+
+}
