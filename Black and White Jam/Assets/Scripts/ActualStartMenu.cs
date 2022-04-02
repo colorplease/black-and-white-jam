88b992524cf5fd34b9761593316dd81555293247
colorplease <74271202+colorplease@users.noreply.github.com>
2022-04-02 13:54:23 -0700

    start menu among us

---
+++ Black and White Jam/Assets/Scripts/ActualStartMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActualStartMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public bool closeMenuMenu;
+    public bool pleaseForTheLoveOfGod;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        closeMenuMenu = false;
+    }
+
+     public void OnPointerExit(PointerEventData eventData)
+    {
+        closeMenuMenu = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            pleaseForTheLoveOfGod = true;
+        }
+        else
+        {
+            pleaseForTheLoveOfGod = false;
+        }
+    }
+}
---
+++ Black and White Jam/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class StartMenu : MonoBehaviour, ISelectHandler, IPointerDownHandler, IDeselectHandler
+{
+    public TextMeshProUGUI startText;
+    public GameObject startMenu;
+    public ActualStartMenu actualStartMenu;
+
+    void Awake()
+    {
+        actualStartMenu = GameObject.Find("Start Menu").GetComponent<ActualStartMenu>();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        startText.color = Color.black;
+        startMenu.SetActive(true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        startText.color = Color.white;
+        if (actualStartMenu.closeMenuMenu == true)
+        {
+            startMenu.SetActive(false);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        startText.color = Color.black;
+    }
+
+    void Update()
+    {
+        if (actualStartMenu.closeMenuMenu == true && actualStartMenu.pleaseForTheLoveOfGod == true)
+        {
+            startText.color = Color.white;
+            actualStartMenu.closeMenuMenu = false;
+            actualStartMenu.pleaseForTheLoveOfGod = false;
+            startMenu.SetActive(false);
+        }
+    }
+}
