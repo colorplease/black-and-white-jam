c797e583a99c41e490408b35c5493090592f3da0
colorplease <74271202+colorplease@users.noreply.github.com>
2022-04-03 10:39:04 -0700

    fish

--- Black and White Jam/Assets/Scripts/Resume.cs
+++ Black and White Jam/Assets/Scripts/Resume.cs
@@ -6,6 +6,10 @@ using TMPro;
 public class Resume : MonoBehaviour
 {
     public TMP_InputField inputField;
+    [SerializeField] GameObject granted;
+    [SerializeField] GameObject denied;
+    [SerializeField] GameObject fish;
+    [SerializeField] GameObject fish1;
 
     void Awake()
     {
@@ -19,12 +23,44 @@ public class Resume : MonoBehaviour
             string passwordTry = inputField.text;
             if (passwordTry.Equals("9215"))
             {
-                print("yes");
+                StartCoroutine(AccessGranted());
+            }
+            else if (passwordTry.Equals("fish"))
+            {
+                StartCoroutine(fish2());
             }
             else
             {
-                print("no");
+                StartCoroutine(AccessDenied());
             }
         }
+    }
+
+    IEnumerator AccessGranted()
+    {
+        fish.SetActive(false);
+        granted.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        fish.SetActive(true);
+        granted.SetActive(false);
+        Destroy(gameObject);
+    }
+
+    IEnumerator AccessDenied()
+    {
+        fish.SetActive(false);
+        denied.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        fish.SetActive(true);
+        denied.SetActive(false);
+    }
+
+    IEnumerator fish2()
+    {
+        fish.SetActive(false);
+        fish1.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        fish.SetActive(true);
+        fish1.SetActive(false);
     }
 }
