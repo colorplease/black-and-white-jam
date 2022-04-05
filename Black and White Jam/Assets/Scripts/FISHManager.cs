f94957f4664b7884edde5121d3b348346fcb1deb
colorplease <74271202+colorplease@users.noreply.github.com>
2022-04-05 13:41:47 -0700

    im too tired for this shit dawg

--- Black and White Jam/Assets/Scripts/FISHManager.cs
+++ Black and White Jam/Assets/Scripts/FISHManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
 
+[System.Serializable]
+public class TutorialDialogue
+{
+    public string tutorialDialogue;
+    public float timeBetweenNextDialogue;
+    public bool breakTime;
+    public int setNumberCall;
+}
+
 public class FISHManager : MonoBehaviour, IPointerDownHandler
 {
     public int maxMessages = 25;
@@ -12,10 +21,27 @@ public class FISHManager : MonoBehaviour, IPointerDownHandler
    List<Message> messageList = new List<Message>();
    [SerializeField] RectTransform dragRectTransform;
    [SerializeField]int currentMessage;
+   [SerializeField]TutorialDialogue[] tutorialDialogues;
+   [SerializeField]int currentDialogue;
+   [SerializeField]bool tutorial;
+   [SerializeField]GameObject[] tutorialObjects;
+   bool updateCall;
+   public bool complete;
+   int callNumber;
+   GameObject[] windows;
+    Window[] windowScripts;
 
    void Awake()
    {
-       SendMessageToChat("> COMPLETE ALL TASKS BY THE END OF THE DAY");
+       if (!tutorial)
+       {
+           SendMessageToChat("> COMPLETE ALL TASKS BY THE END OF THE DAY");
+       }
+       else
+       {
+           StartCoroutine(Set());
+       }
+       
    }
 
    public void SendMessageToChat(string text)
@@ -43,6 +69,124 @@ public class FISHManager : MonoBehaviour, IPointerDownHandler
    public void OnPointerDown(PointerEventData eventData)
     {
         dragRectTransform.SetAsLastSibling();
+    }
+
+    void Sets(int setNumber)
+    {
+        switch(setNumber)
+        {
+            case 0:
+            currentDialogue++;
+            tutorialObjects[0].SetActive(true);
+            tutorialObjects[1].SetActive(true);
+            tutorialObjects[2].SetActive(true);
+            StartCoroutine(Set());
+            break;
+
+            case 1:
+            currentDialogue++;
+            updateCall = true;
+            callNumber = setNumber;
+            break;
+
+            case 2:
+            currentDialogue++;
+            updateCall = true;
+            callNumber = setNumber;
+            break;
+
+            case 3:
+            currentDialogue++;
+            tutorialObjects[0].SetActive(false);
+            tutorialObjects[3].SetActive(true);
+            windows = GameObject.FindGameObjectsWithTag("Window");
+        windowScripts = new Window[windows.Length];
+        for (int i = 0; i < windows.Length; i++)
+        {
+            windowScripts[i] = windows[i].GetComponent<Window>();
+            windowScripts[i].Close();
+        }
+            break;
+            
+            case 4:
+            currentDialogue++;
+            updateCall = true;
+            callNumber = setNumber;
+            break;
+
+            case 5:
+            currentDialogue++;
+            updateCall = true;
+            callNumber = setNumber;
+            break;
+
+
+        }
+    }
+
+    void Update()
+    {
+        if (updateCall)
+        {
+            switch(callNumber)
+            {
+                case 1:
+                    if (GameObject.FindGameObjectWithTag("Window") != null)
+                    {
+                        StartCoroutine(Set());
+                        updateCall = false;
+                    }
+                break;
+
+                case 2:
+                if (GameObject.FindGameObjectWithTag("circle") == null)
+                {
+                    StartCoroutine(Set());
+                    updateCall = false;
+                }
+                break;
+                
+                case 3:
+                if (GameObject.FindGameObjectWithTag("Window") != null)
+                    {
+                        StartCoroutine(Set());
+                        updateCall = false;
+                    }
+                break;
+
+                case 4:
+                if (complete)
+                    {
+                        StartCoroutine(Set());
+                        updateCall = false;
+                    }
+                break;
+
+                case 5:
+                if (GameObject.FindGameObjectWithTag("Window") == null)
+                    {
+                        StartCoroutine(Set());
+                        updateCall = false;
+                    }
+                break;
+            }
+        }
+    }
+
+    IEnumerator Set()
+    {
+        SendMessageToChat(tutorialDialogues[currentDialogue].tutorialDialogue);
+        yield return new WaitForSeconds(tutorialDialogues[currentDialogue].timeBetweenNextDialogue);
+        if(tutorialDialogues[currentDialogue].breakTime)
+        {
+            Sets(tutorialDialogues[currentDialogue].setNumberCall);
+        }
+        else
+        {
+            currentDialogue++;
+            StartCoroutine(Set());
+        }
+        
     }
 
 
---
+++ Black and White Jam/Assets/Scripts/tutorialEnd.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class tutorialEnd : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    [SerializeField] GameObject granted;
+    [SerializeField] GameObject denied;
+    [SerializeField] TaskManager taskManager;
+    [SerializeField] FISHManager fishManager;
+    public GameObject windowToOpen;
+    public RectTransform Canvas;
+    public Transform shakeaShakea;
+    bool alreadyComplete;
+    
+
+
+    void Awake()
+    {
+        taskManager = GameObject.FindGameObjectWithTag("TaskManager").GetComponent<TaskManager>();
+        inputField.characterLimit = 4;
+        Canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        shakeaShakea = GameObject.FindGameObjectWithTag ("ShakeaShakea").GetComponent<RectTransform>();
+        fishManager = GameObject.FindGameObjectWithTag("FISH").GetComponent<FISHManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            string passwordTry = inputField.text;
+            if (passwordTry.Equals("6822"))
+            {
+                StartCoroutine(AccessGranted());
+            }
+            else
+            {
+                StartCoroutine(AccessDenied());
+                taskManager.Mistake(0);
+                fishManager.SendMessageToChat("> KatF!sh: Usually when you make a mistake, time gets deducted from your timer, but you're still learning so i won't do that");
+            }
+        }
+    }
+
+    IEnumerator AccessGranted()
+    {
+        granted.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        granted.SetActive(false);
+        GameObject window = Instantiate(windowToOpen, new Vector3(Random.Range(-100, 100) / shakeaShakea.localScale.x, Random.Range(-87, 155), 0)/shakeaShakea.localScale.y, transform.rotation);
+        window.transform.localScale = new Vector3(window.transform.localScale.x / shakeaShakea.localScale.x, window.transform.localScale.y / shakeaShakea.localScale.y, 1f);
+        window.transform.SetParent(shakeaShakea); 
+        taskManager.TaskComplete(1);
+        if (!alreadyComplete)
+        {
+            fishManager.SendMessageToChat("> Task Completed! [Complete Tutorial]");
+            fishManager.complete = true;
+            alreadyComplete = true;
+        }
+        Destroy(gameObject);
+    }
+
+    IEnumerator AccessDenied()
+    {
+        denied.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        denied.SetActive(false);
+    }
+
+}
