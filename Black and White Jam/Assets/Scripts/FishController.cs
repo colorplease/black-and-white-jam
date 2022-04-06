cf9795b05e2926f26c4023a57f2a822b19793c5b
colorplease <74271202+colorplease@users.noreply.github.com>
2022-04-06 10:56:56 -0700

    good enough

--- Black and White Jam/Assets/Scripts/FishController.cs
+++ Black and White Jam/Assets/Scripts/FishController.cs
@@ -13,6 +13,10 @@ public class FishController : MonoBehaviour
     [SerializeField]GameObject trash;
     [SerializeField]GameObject trashFish;
     [SerializeField]GameObject trashText;
+    [SerializeField]Transform camera;
+    [SerializeField]float[] xLimits;
+    [SerializeField]float[] xPos;
+    [SerializeField]int limit;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,6 +46,23 @@ public class FishController : MonoBehaviour
             animator.SetBool("isMoving", false);
         }
 
+        if (xLimits[limit] < transform.position.x)
+        {
+            if (camera.position != new Vector3 (xPos[limit + 1], camera.position.y, camera.position.z))
+            {
+                limit++;
+                camera.position = new Vector3 (xPos[limit], camera.position.y, camera.position.z);
+            }
+        }
+        if (xLimits[limit - 1] > transform.position.x)
+        {
+            if (camera.position != new Vector3 (xPos[limit - 1 ], camera.position.y, camera.position.z))
+            {
+                limit--;
+                camera.position = new Vector3 (xPos[limit], camera.position.y, camera.position.z);
+            }
+        }
+
         
     }
 
@@ -59,4 +80,5 @@ public class FishController : MonoBehaviour
             trash.SetActive(true);
         }
     }
+    
 }
